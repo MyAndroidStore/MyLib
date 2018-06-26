9037ddb6189227228476c8dba28c38da45dea7ed
Pikachu <55527267@qq.com>
2018-06-26 19:39:49 +0800

    2.0.3

--- androidlib/src/main/java/org/wjh/androidlib/nohttp/NoHttpCallBack.java
+++ androidlib/src/main/java/org/wjh/androidlib/nohttp/NoHttpCallBack.java
@@ -44,7 +44,33 @@ public abstract class NoHttpCallBack implements OnResponseListener<String> {
 
     @Override
     public void onSucceed(int what, Response<String> response) {
-        onSucceed(response.get());
+
+        if (response.responseCode() == 200) {
+            onSucceed(response.get());
+        }else {
+
+            if (mActivity != null) {
+
+                Exception exception = response.getException();
+                if (exception instanceof NetworkError) {// 网络不好
+                    ToastUtils.getInstance().shortToast(R.string.error_please_check_network);
+                } else if (exception instanceof TimeoutError) {// 请求超时
+                    ToastUtils.getInstance().shortToast(R.string.error_timeout);
+                } else if (exception instanceof UnKnownHostError) {// 找不到服务器
+                    ToastUtils.getInstance().shortToast(R.string.error_not_found_server);
+                } else if (exception instanceof URLError) {// URL是错的
+                    ToastUtils.getInstance().shortToast(R.string.error_url_error);
+                } else if (exception instanceof NotFoundCacheError) {
+                    // 这个异常只会在仅仅查找缓存时没有找到缓存时返回
+                    // 没有缓存一般不提示用户，如果需要随你。
+                } else {
+                    ToastUtils.getInstance().shortToast("出错了");
+                }
+            }
+
+            onFailed();
+        }
+
     }
 
     @Override
