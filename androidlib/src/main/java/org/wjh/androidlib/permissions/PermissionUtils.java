19dbd629b53205b37f3b43ffb2502f61c61b65e6
MyAndroidStore <2019880507@qq.com>
2018-07-24 09:08:29 +0800

    2.2.8

--- androidlib/src/main/java/org/wjh/androidlib/permissions/PermissionUtils.java
+++ androidlib/src/main/java/org/wjh/androidlib/permissions/PermissionUtils.java
@@ -2,6 +2,10 @@ package org.wjh.androidlib.permissions;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.hardware.Camera;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 
@@ -11,6 +15,7 @@ import com.yanzhenjie.permission.Setting;
 
 import org.wjh.androidlib.utils.ToastUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class PermissionUtils {
@@ -109,4 +114,82 @@ public class PermissionUtils {
                 })
                 .start();
     }
+
+
+    public static boolean isCameraCanUse() {
+        boolean canUse = true;
+        Camera mCamera = null;
+        try {
+            mCamera = Camera.open();
+            // setParameters 是针对魅族MX5 做的。MX5 通过Camera.open() 拿到的Camera
+            // 对象不为null
+            Camera.Parameters mParameters = mCamera.getParameters();
+            mCamera.setParameters(mParameters);
+        } catch (Exception e) {
+            canUse = false;
+        }
+        if (mCamera != null) {
+            mCamera.release();
+        }
+        return canUse;
+    }
+
+
+    public boolean isHasPermission() {
+        Field fieldPassword = null;
+        try {
+            Camera camera = Camera.open();
+            fieldPassword = camera.getClass().getDeclaredField("mHasPermission");
+            fieldPassword.setAccessible(true);
+            return (boolean) fieldPassword.get(camera);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return true;
+        }
+
+    }
+
+
+    /**
+     * 判断是是否有录音权限
+     */
+    public static boolean isHasRecordPermission(final Context context) {
+        // 音频获取源
+        int audioSource = MediaRecorder.AudioSource.MIC;
+        // 设置音频采样率，44100是目前的标准，但是某些设备仍然支持22050，16000，11025
+        int sampleRateInHz = 44100;
+        // 设置音频的录制的声道CHANNEL_IN_STEREO为双声道，CHANNEL_CONFIGURATION_MONO为单声道
+        int channelConfig = AudioFormat.CHANNEL_IN_STEREO;
+        // 音频数据格式:PCM 16位每个样本。保证设备支持。PCM 8位每个样本。不一定能得到设备支持。
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        // 缓冲区字节大小
+        int bufferSizeInBytes = 0;
+
+
+        bufferSizeInBytes = 0;
+        bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz,
+                channelConfig, audioFormat);
+        AudioRecord audioRecord = new AudioRecord(audioSource, sampleRateInHz,
+                channelConfig, audioFormat, bufferSizeInBytes);
+        //开始录制音频
+        try {
+            // 防止某些手机崩溃，例如联想
+            audioRecord.startRecording();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+        /**
+         * 根据开始录音判断是否有录音权限
+         */
+        if (audioRecord.getRecordingState() != AudioRecord.RECORDSTATE_RECORDING) {
+            return false;
+        }
+        audioRecord.stop();
+        audioRecord.release();
+        audioRecord = null;
+
+        return true;
+    }
+
+
 }
